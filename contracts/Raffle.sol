5ca63aa0b1965a1a5d04e74627d846057b5de0d8
Priyanshu Desai <desai09p@gmail.com>
2026-05-04 18:49:26 +0100

    chore: implement constructor and ticket price retrieval in Raffle contract

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,8 +2,17 @@
 pragma solidity ^0.8.18;
 
 contract Raffle {
+    uint256 private immutable i_ticketPrice;
 
-    // function enter(){}
+    constructor(uint256 _ticketPrice) {
+        i_ticketPrice = _ticketPrice;
+    }
+
+    function enter(){}
 
     // function winner(){}
+
+    function getTicketPrice() public view returns (uint256) {
+        return i_ticketPrice;
+    }
 }
