f9b308689df3f504c54114cd1a2f60948614b34d
Priyanshu Desai <desai09p@gmail.com>
2026-05-04 18:58:22 +0100

    chore: add player tracking and retrieval in Raffle contract

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -5,20 +5,26 @@ error Raffle__NotEnoughEthSent();
 
 contract Raffle {
     uint256 private immutable i_ticketPrice;
+    address payable[] private s_players;
 
     constructor(uint256 _ticketPrice) {
         i_ticketPrice = _ticketPrice;
     }
 
-    function enter(){
+    function enter() public payable{
         if (msg.value < i_ticketPrice) {
             revert Raffle__NotEnoughEthSent();
         }
+        s_players.push(payable(msg.sender));
     }
 
     // function winner(){}
 
     function getTicketPrice() public view returns (uint256) {
         return i_ticketPrice;
+    }
+
+    function getPlayer(uint256 index) public view returns (address payable) {
+        return s_players[index];
     }
 }
