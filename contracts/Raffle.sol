a480ecb719546275fbdfe946fb60f2aceb71e030
Priyanshu Desai <desai09p@gmail.com>
2026-05-18 18:41:37 +0100

    chore: update dependencies in package.json
    - Added @chainlink/contracts version 1.5.0
    - Added @nomicfoundation/hardhat-toolbox version 7.0.0

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,15 +1,20 @@
 // SPDX-LICENSE-IDENTIFIER: MIT
 pragma solidity ^0.8.18;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 error Raffle__NotEnoughEthSent();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     uint256 private immutable i_ticketPrice;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
 
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 _ticketPrice) {
+    constructor(address _vrfCoordinator, uint256 _ticketPrice) VRFConsumerBaseV2Plus(_vrfCoordinator) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinator);
         i_ticketPrice = _ticketPrice;
     }
 
@@ -21,7 +26,20 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    // function winner(){}
+    function requestWinner() external {
+        i_vrfCoordinator.requestRandomWords(
+            // Parameters for the VRF request
+            keyHash,
+            subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+    }
+
+    function fullfillRandomWords(uint256 _requestId, uint256[] memory _randomWords) internal override {
+        // Logic to determine the winner using the random words
+    }
 
     function getTicketPrice() public view returns (uint256) {
         return i_ticketPrice;
